ec9ca734f9ae39f26fe25eff8e5d49222f97ce1b
antropushka <97852620+antropushka@users.noreply.github.com>
2022-09-29 11:38:56 +0300

    methods

--- src/by/epam/lesson/hierarchy/view/ConsoleOutPutter.java
+++ src/by/epam/lesson/hierarchy/view/ConsoleOutPutter.java
@@ -1,6 +1,9 @@
 package by.epam.lesson.hierarchy.view;
 
 import by.epam.lesson.hierarchy.bean.electronic_device.ElectronicDevice;
+import by.epam.lesson.hierarchy.bean.electronic_device.air_cond_device.AirConditioningDevice;
+import by.epam.lesson.hierarchy.bean.electronic_device.clothes_care_device.ClothesCareDevice;
+import by.epam.lesson.hierarchy.bean.electronic_device.home_device.HomeDevice;
 
 public class ConsoleOutPutter {
 
@@ -13,7 +16,15 @@ public class ConsoleOutPutter {
         return instance;
     }
 
-    public void printInfoAbout(ElectronicDevice electronicDevice) {
-        System.out.println(electronicDevice);
+    public void printInfoAbout(AirConditioningDevice airConditioningDevice) {
+        System.out.println(airConditioningDevice);
+    }
+
+    public void printInfoAbout(ClothesCareDevice clothesCareDevice) {
+        System.out.println(clothesCareDevice);
+    }
+
+    public void printInfoAbout(HomeDevice homeDevice) {
+        System.out.println(homeDevice);
     }
 }
